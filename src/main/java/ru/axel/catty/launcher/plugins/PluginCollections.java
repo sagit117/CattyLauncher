c6c413643fda57b39a48a961c3a3e4aab3ff49d2
Pavel Aks <sagit117@gmail.com>
2022-11-07 13:06:07 +0300

    add pair

--- src/main/java/ru/axel/catty/launcher/plugins/PluginCollections.java
+++ src/main/java/ru/axel/catty/launcher/plugins/PluginCollections.java
@@ -4,11 +4,13 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import ru.axel.catty.engine.headers.Headers;
 import ru.axel.catty.engine.routing.RouteExecute;
+import ru.axel.catty.launcher.utilities.Pair;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class PluginCollections {
     /**
@@ -16,19 +18,21 @@ public final class PluginCollections {
      * @return метод плагина.
      */
     @Contract(pure = true)
-    public static @NotNull RouteExecute formParametersParser() {
+    public static @NotNull RouteExecute formParametersParser(Logger logger) {
         return (request, response) -> {
             if (Objects.equals(request.getHeaders(Headers.CONTENT_TYPE), "application/x-www-form-urlencoded")) {
                 final var splitBody = request.getBody().split("&");
 
                 Arrays.stream(splitBody).forEach(parameters -> {
-                    final var splitParameters = parameters.split("=");
+                    final Pair<String, String> pair = new Pair<>(parameters.split("="));
+
+                    logger.finest(
+                      "Установка параметров в запрос: " + pair
+                    );
 
                     request.setParams(
-                        splitParameters[0],
-                        splitParameters.length > 1
-                            ? URLDecoder.decode(splitParameters[1], StandardCharsets.UTF_8)
-                            : null
+                        pair.getKey(),
+                        URLDecoder.decode(pair.getValue(), StandardCharsets.UTF_8)
                     );
                 });
             }
---
+++ src/main/java/ru/axel/catty/launcher/utilities/Pair.java
@@ -0,0 +1,34 @@
+package ru.axel.catty.launcher.utilities;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Contract(pure = true)
+    @SuppressWarnings("unchecked")
+    public Pair(String @NotNull [] arr) {
+        key = (K) arr[0];
+        value = (V) arr[1];
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Contract(pure = true)
+    public @NotNull String toString() {
+        return "[key=" + key + ", value=" + value + "]";
+    }
+}
