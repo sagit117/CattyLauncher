823e3abd6d7e946da7654077ff00078aeb5e5760
Pavel Aks <sagit117@gmail.com>
2022-11-12 12:12:13 +0300

    add factory route method options

--- src/main/java/ru/axel/catty/launcher/factory/RouteFactory.java
+++ src/main/java/ru/axel/catty/launcher/factory/RouteFactory.java
@@ -1,17 +1,23 @@
 package ru.axel.catty.launcher.factory;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import ru.axel.catty.engine.routing.ICattyRoute;
 import ru.axel.catty.engine.routing.Route;
 import ru.axel.catty.engine.routing.RouteExecute;
 
+/**
+ * Класс фабрика для создания маршрутов
+ */
 final public class RouteFactory {
-    @Contract("_, _ -> new")
     public static @NotNull ICattyRoute get(String path, RouteExecute execute) {
         return new Route(path, "GET", execute);
     }
+
     public static @NotNull ICattyRoute post(String path, RouteExecute execute) {
         return new Route(path, "POST", execute);
+    }
+
+    public static @NotNull ICattyRoute options(String path, RouteExecute execute) {
+        return new Route(path, "OPTIONS", execute);
     }
 }
